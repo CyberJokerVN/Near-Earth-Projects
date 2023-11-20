ad55e3fd39ac98b81dc80bbbf762e48de9e7105e
CyberJokerVN <CyberJokerVN@proton.me>
2023-11-20 22:55:02 +0900

    Finish code

--- models.py
+++ models.py
@@ -32,7 +32,7 @@ class NearEarthObject:
     initialized to an empty collection, but eventually populated in the
     `NEODatabase` constructor.
     """
-    # If you make changes, be sure to update the comments in this file.
+
     def __init__(self, designation, hazardous, name, diameter):
         """Create a new `NearEarthObject`.
 
@@ -64,7 +64,7 @@ class NearEarthObject:
         """Return `str(self)`."""
         # The project instructions include one possibility. Peek at the __repr__
         # method for examples of advanced string formatting.
-        return(
+        return (
             f"NearEarthObject(designation={self.designation!r}, "
             f"name={self.name!r}, diameter={self.diameter:.3f}, hazardous={self.hazardous!r})"
         )
@@ -76,7 +76,7 @@ class NearEarthObject:
             f"name={self.name!r}, diameter={self.diameter:.3f}, hazardous={self.hazardous!r})"
         )
 
-    #Create JSON serializable object
+    # Create JSON serializable object
     def serialize(self):
         """Return a dictionary representation of this object."""
         return {
@@ -100,17 +100,8 @@ class CloseApproach:
     private attribute, but the referenced NEO is eventually replaced in the
     `NEODatabase` constructor.
     """
-    # TODO: How can you, and should you, change the arguments to this constructor?
-    # If you make changes, be sure to update the comments in this file.
-    def __init__(self, designation, time, distance, velocity):
-        """Create a new `CloseApproach`.
 
-        :param info: A dictionary of excess keyword arguments supplied to the constructor.
-        """
-        # TODO: Assign information from the arguments passed to the constructor
-        # onto attributes named `_designation`, `time`, `distance`, and `velocity`.
-        # You should coerce these values to their appropriate data type and handle any edge cases.
-        # The `cd_to_datetime` function will be useful.
+    def __init__(self, designation, time, distance, velocity):
         self.designation = designation
         self.time = cd_to_datetime(time)
         self.distance = distance
@@ -152,7 +143,7 @@ class CloseApproach:
             f"velocity={self.velocity:.2f}, neo={self.neo!r})"
         )
 
-    #Create JSON serializable object
+    # Create JSON serializable object
     def serialize(self):
         return {
             "datetime_utc": datetime_to_str(self.time),
